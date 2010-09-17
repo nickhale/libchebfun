4bde124faa9faad834514108d56cf5ee5814fc09
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-09-17 13:49:42 +0000

    since we're not using ax_blas or ax_lapack, no need for this.

--- autogen.sh
+++ autogen.sh
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# run aclocal with the right includes
-aclocal -Im4/
+# run aclocal
+aclocal
 
 # run autoconf
 autoconf
