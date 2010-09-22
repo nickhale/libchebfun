c3c4c836fc1a937f7634da95e2689f8cb86272a3
gonnet <gonnet@0b802cf0-55e5-418a-bfd8-3c87ced2b317>
2010-09-22 14:37:40 +0000

    new error-handling framework.

---
+++ src/errs.h
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * This file is part of libchebfun.
+ * Coypright (c) 2010 The Chebfun Team.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ ******************************************************************************/
+
+/* Some defines. */
+#define errs_maxstack                           100
+
+#define errs_err_ok                             0
+#define errs_err_io                             -1
+
+
+/* Global variables. */
+extern int errs_err;
+extern const char *errs_err_msg[];
+
+
+/* Functions. */
+int errs_register( int id , const char *msg , int line , char *file );
+int errs_dump( FILE *out );
+void errs_clear( );
